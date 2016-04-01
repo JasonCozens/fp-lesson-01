46ae6d5b8e380b021894a5b1e262c612e8abb76f
wp <e.jason.cozens@gmail.com>
2016-04-01 12:37:24 +0100

    Basis for a single page site with menus at top and bottom.

--- themes/fpl01/index.php
+++ themes/fpl01/index.php
@@ -11,6 +11,12 @@
         <h1>Introduction</h1>
         <p>This course will teach you FoundationPress in great detail.</p>
     </div>
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+            the_content();
+        endwhile;
+        ?>
     <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
 </body>
 </html>
