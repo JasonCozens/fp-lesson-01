c90d00b88088e4d6403bcb32dcda3b384c8f44f8
Jason Cozens <e.jason.cozens@gmail.com>
2016-04-12 20:42:53 +0100

    Added reset to style.css

--- themes/ejc-one/index.php
+++ themes/ejc-one/index.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>FP Express</title>
+    <meta charset="UTF-8">
+    <title>FP Express</title>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_uri(); ?>" />
 </head>
 <body>
   <h1>FoundationPress Express</h1>
